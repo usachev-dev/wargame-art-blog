0b39fc659bd3eecf15f96b9a3d0dde52710b118f
Alexey Usachev <usachev.dev@gmail.com>
2017-09-02 09:49:37 +0300

    dsf

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var routes = require('./app_server/routes');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'components'));
+//app.set('views', path.join(__dirname, 'app_server', 'components'));
+app.set('views', path.join('app_server', 'components'));
 app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
