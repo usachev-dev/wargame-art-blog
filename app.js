e77083b2b37a64c35463c9985d830cb4ca4781f2
Alexey Usachev <usachev.dev@gmail.com>
2017-09-02 09:44:41 +0300

    dsf

--- app.js
+++ app.js
@@ -35,11 +35,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
   //console.error(err.stack);
   // render the error page
+  res.locals.error = err;
   res.status(err.status || 500);
-  res.render('pages/error');
+  res.render('/pages/error');
 });
 
 module.exports = app;
